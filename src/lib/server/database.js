0bb9420017b28dc8a2591be462566a340d5c1fcb
kkaushik03 <111904079+kkaushik03@users.noreply.github.com>
2024-10-22 11:54:44 -0700

    Update database connection to use Heroku's DATABASE_URL

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -1,16 +1,15 @@
 import postgres from 'postgres';
 
-// Use DATABASE_URL instead of PGCONNECT from your Heroku environment
-const PGCONNECT = process.env.DATABASE_URL; // Correctly access the environment variable
+const PGCONNECT = process.env.DATABASE_URL || 'postgres://udrskkfeujed89:pdf40e847bd0a225779abcea6f2712f8457f8c01b4f9c2f81bdffb10fc6a26c99@cat670aihdrkt1.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d7oa3f76jp43lc';
 
 // Check if the environment variable is available
 if (!PGCONNECT) {
     throw new Error('Missing database connection string!');
 }
 
-// Create the SQL client using the connection string and enable SSL if needed (Heroku generally requires SSL for Postgres)
+// Create the SQL client using the connection string
 const sql = postgres(PGCONNECT, {
-    ssl: { rejectUnauthorized: false } // Set to false to allow self-signed certificates
+    ssl: { rejectUnauthorized: false } // Allow SSL self-signed certificates
 });
 
 export default sql;
